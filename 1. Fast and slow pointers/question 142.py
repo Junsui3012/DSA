910e3de503e86577098f3170e8aaac9f087b7bb4
Junsui3012 <sajals8909@gmail.com>
2025-11-05 16:18:49 +0530

    first question

---
+++ 1. Fast and slow pointers/question 142.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        t=h=head
+        cycle=False
+        while h!=None and h.next!=None:
+            t=t.next
+            h=h.next.next
+            if t==h:
+                cycle=True
+                break
+        if not cycle:
+            return None
+        t2=head
+        while t2!=h:
+            t2=t2.next
+            h=h.next
+        return t2
